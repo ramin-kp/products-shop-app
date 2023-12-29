cef53ea2371c6389805e8f4fe9d28e6fef7afdaf
Ramin Karimpor <ramin.kp81@gmail.com>
2023-12-29 04:04:33 +0330

    feat:developing aside

--- src/context/ProductContext.jsx
+++ src/context/ProductContext.jsx
@@ -8,7 +8,6 @@ export default function ProductContext({ children }) {
       try {
         setProducts(await api.get("/products"));
       } catch (error) {
-        console.log(error.message);
       }
     };
     getData();
--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,6 +1,7 @@
 import { useProducts } from "../context/ProductContext";
 import Loader from "./../components/Loader";
 import { CiSearch } from "react-icons/ci";
+import { FaList } from "react-icons/fa";
 import Product from "../components/Product";
 import { useEffect, useState } from "react";
 import Headers from "../components/Headers";
@@ -17,10 +18,9 @@ export default function Products() {
       product.title.toLowerCase().includes(text)
     );
     setShowProducts(data);
-    console.log(data);
   };
   return (
-    <div className="container">
+    <main className="container">
       <Headers />
       <div className="flex items-center gap-x-2 my-6">
         <input
@@ -34,17 +34,44 @@ export default function Products() {
           <CiSearch className="shrink-0 text-xl" onClick={searchHandler} />
         </button>
       </div>
+      <div className="flex items-start gap-x-5">
+        <section className="grow">
+          {showProducts.length > 1 ? (
+            <div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-5 md:gap-16 p-5">
+              {showProducts.map((product) => (
+                <Product key={product.id} product={product} />
+              ))}
+            </div>
+          ) : (
+            <Loader />
+          )}
+        </section>
+        <aside className="p-5 mt-5 border border-dashed border-rose-600 rounded-md">
+          <div className="flex items-end gap-2.5 mb-2.5 text-rose-600 text-2xl font-medium">
+            <FaList />
+            categories
+          </div>
+          <ul className="space-y-2 text-base font-medium child:cursor-pointer">
+            <li className="p-1 hover:bg-rose-400 hover:text-white transition-all duration-300 rounded">
+              All
+            </li>
+            <li className="p-1 hover:bg-rose-400 hover:text-white transition-all duration-300 rounded">
+              Electronics
+            </li>
+            <li className="p-1 hover:bg-rose-400 hover:text-white transition-all duration-300 rounded">
+              Jewelry
+            </li>
+            <li className="p-1 hover:bg-rose-400 hover:text-white transition-all duration-300 rounded">
+              Men's clothing
+            </li>
+            <li className="p-1 hover:bg-rose-400 hover:text-white transition-all duration-300 rounded">
+              Women's clothing
+            </li>
+          </ul>
+        </aside>
+      </div>
 
-      {showProducts.length > 1 ? (
-        <div className="grid grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5 md:gap-16 p-5">
-          {showProducts.map((product) => (
-            <Product key={product.id} product={product} />
-          ))}
-        </div>
-      ) : (
-        <Loader />
-      )}
       <Footer />
-    </div>
+    </main>
   );
 }
